d7b5df1f8d673f0329023500b39aa30c83c75021
Roberto Verdugo Beltran <roberto.verdugo248285@potros.itson.edu.mx>
2024-09-26 20:32:00 -0700

    Update PeliculasDAO.java

--- src/main/java/Persistencia/PeliculasDAO.java
+++ src/main/java/Persistencia/PeliculasDAO.java
@@ -22,6 +22,46 @@ public class PeliculasDAO implements IPeliculasDAO {
     }
 
     @Override
+<<<<<<< Updated upstream
+=======
+    public ArrayList<PeliculasDTO> leer() throws PersistenciaException {
+        PeliculasDTO peliculaDTO;
+        Connection con = null;
+        ResultSet rs;
+        ArrayList<PeliculasDTO> lista = new ArrayList<>();
+
+        try {
+            con = conexionBD.crearConexion();
+            String leer = "SELECT * FROM peliculas";
+            PreparedStatement ps = con.prepareStatement(leer);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                peliculaDTO = new PeliculasDTO();
+                peliculaDTO.setId(rs.getInt("id"));
+                peliculaDTO.setTitulo(rs.getString("titulo")); // Cambiado a minúscula
+                peliculaDTO.setClasificacion(rs.getString("clasificacion")); // Cambiado para asignar correctamente
+                peliculaDTO.setGenero(rs.getString("genero")); // Añadido
+                peliculaDTO.setDuracionMinutos(rs.getInt("duracionMinutos"));
+                peliculaDTO.setSinopsis(rs.getString("sinopsis")); // Asegúrate de que el nombre sea correcto
+                peliculaDTO.setLinkTrailer(rs.getString("linkTrailer"));
+                peliculaDTO.setRutaImagen(rs.getString("rutaImagen"));
+                lista.add(peliculaDTO);
+            }
+
+            rs.close();
+            ps.close();
+            con.close(); // Mover fuera del while
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conexion: " + e.getMessage());
+            e.printStackTrace(); // Añadir traza de error
+        }
+        return lista;
+    }
+
+    
+    @Override
+>>>>>>> Stashed changes
     public void guardar(Peliculas pelicula) throws PersistenciaException {
         String sql = "INSERT INTO peliculas (titulo, clasificacion, genero,  paisOrigen, duracionMinutos,sinopsis,linkTrailer) VALUES (?, ?, ?, ?, ?, ?,?)";
         try (Connection conexion = conexionBD.crearConexion(); PreparedStatement prepared = conexion.prepareStatement(sql)) {
